b233b16d88dd2cb7cfd4f91fd834fe42650dbdc7
Sarah Robinson <sarahrose@Sarahs-MacBook-Pro-4.local>
2023-12-07 11:36:22 +0000

    changing splat operator from serves, loadbalancers, vpc

--- modules/loadbalancer/main.tf
+++ modules/loadbalancer/main.tf
@@ -9,24 +9,24 @@ resource "aws_lb" "lb_main" {
 
 resource "aws_lb_target_group" "target_group" {
   count            = length(var.service_names)
-  name             = "target-group-${element(var.service_names[*], count.index)}"
+  name             = "target-group-${var.service_names[count.index]}"
   port             = 3000
   protocol         = "HTTP"
   protocol_version = "HTTP1"
   vpc_id           = var.vpc_id
   health_check {
-    path     = "/api/${element(var.target_group_paths[*], count.index)}"
+    path     = "/api/${var.target_group_paths[count.index]}"
     protocol = "HTTP"
   }
   tags = {
-    Name = "target-group-${element(var.service_names[*], count.index)}"
+    Name = "target-group-${var.service_names[count.index]}"
   }
 }
 
 resource "aws_lb_target_group_attachment" "target_group_servers_attachment" {
   count            = length(var.server_instance_ids)
-  target_group_arn = "${element(aws_lb_target_group.target_group[*], count.index)}".arn
-  target_id        = element(var.server_instance_ids[*], count.index)
+  target_group_arn = aws_lb_target_group.target_group[count.index].arn
+  target_id        = var.server_instance_ids[count.index]
   port             = 3000
 }
 
@@ -47,16 +47,15 @@ resource "aws_lb_listener" "lb_listener" {
 resource "aws_lb_listener_rule" "listener_rule" {
     count = 2
     listener_arn = aws_lb_listener.lb_listener.arn
-    priority = 100
 
     action {
       type = "forward"
-      target_group_arn = "${element(aws_lb_target_group.target_group[*], count.index)}".arn
+      target_group_arn = aws_lb_target_group.target_group[count.index].arn
     }
 
     condition {
       path_pattern {
-        values = ["/${element(var.target_group_paths[*], count.index)}*"]
+        values = ["/api/${var.target_group_paths[count.index]}*"]
       }
     }
 }
--- modules/servers/main.tf
+++ modules/servers/main.tf
@@ -1,14 +1,14 @@
 resource "aws_instance" "servers" {
   count                       = length(var.ami_ids)
-  ami                         = element(var.ami_ids[*], count.index)
+  ami                         = var.ami_ids[count.index]
   instance_type               = var.instance_type
-  subnet_id                   = element(var.public_subnet_ids[*], count.index)
+  subnet_id                   = var.public_subnet_ids[count.index]
   associate_public_ip_address = true
   vpc_security_group_ids      = var.security_groups_ids
   key_name                    = var.key_name
 
   tags = {
-    Name = "${element(var.service_names[*], count.index)}"
+    Name = "server_${var.service_names[count.index]}"
   }
 }
 
